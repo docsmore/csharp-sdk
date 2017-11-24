1f97c317ccdb6d8152136fc417093630c8942888
docsmore <git@apimatic.io>
2017-11-24 04:55:03 +0000

    initial

---
+++ DOCSMOREAPI.PCL/Configuration.cs
@@ -0,0 +1,11 @@
+using DOCSMOREAPI.PCL.Models;
+namespace DOCSMOREAPI.PCL
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "https://api.docsmore.com";
+
+    }
+}
